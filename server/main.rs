4e110fd3da9d1dc88d3905e717bddf216ffce0df
Александр <Sp1vaks@yandex.ru>
2025-06-07 23:07:07 +0300

    up

--- server/main.rs
+++ server/main.rs
@@ -6,8 +6,9 @@ use std::env;
 mod services; 
 use services::auth_service::{MyAuthService, AuthServiceServer};
 use services::seacrh_service::{MySearchService, SearchServiceServer};
-use services::statusUser_service::{MyStatusService, StatusServiceServer};
+use services::status_user_service::{MyStatusService, StatusServiceServer};
 use services::relationships_service::{MyRelationshipService, RelationshipServiceServer};
+use services::chat_service::{MyChatsService, ChatServiceServer};
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -22,13 +23,15 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let service_search = MySearchService::new(db.clone());
     let service_status = MyStatusService::new(db.clone());
     let service_relationship = MyRelationshipService::new(db.clone());
+    let service_chat = MyChatsService::new(db.clone());
 
-    println!("AuthService running on {}", addr);
+    println!("Services running on {}", addr);
     Server::builder()
         .add_service(AuthServiceServer::new(service_auth))
         .add_service(SearchServiceServer::new(service_search))
         .add_service(StatusServiceServer::new(service_status))
         .add_service(RelationshipServiceServer::new(service_relationship))
+        .add_service(ChatServiceServer::new(service_chat))
         .serve(addr)
         .await?;
 
