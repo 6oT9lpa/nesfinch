15d9571b4212c3336ca2b8c4a832efb0031870a6
Александр <Sp1vaks@yandex.ru>
2025-06-07 23:10:52 +0300

    123

--- client/src/clientgRPC.js
+++ client/src/clientgRPC.js
@@ -9,7 +9,8 @@ const { rejects } = require('assert');
 const PROTO_PATHS = [
     path.join(__dirname, '../../proto/service_auth.proto'),
     path.join(__dirname, '../../proto/service_communication.proto'),
-    path.join(__dirname, '../../proto/service_status.proto')
+    path.join(__dirname, '../../proto/service_status.proto'),
+    path.join(__dirname, '../../proto/service_chat.proto')
 ];
 
 const packageDefinition = protoLoader.loadSync(PROTO_PATHS, {
@@ -26,11 +27,13 @@ const AuthService = protoDescriptor.auth.AuthService;
 const RelationshipService = protoDescriptor.communication.RelationshipService;
 const SearchService = protoDescriptor.communication.SearchService;
 const StatusService = protoDescriptor.status.StatusService;
+const ChatService = protoDescriptor.chats.ChatService;
 
 const client_auth = new AuthService('localhost:50051', grpc.credentials.createInsecure());
 const client_search = new SearchService('localhost:50051', grpc.credentials.createInsecure());
 const client_status = new StatusService('localhost:50051', grpc.credentials.createInsecure());
 const client_relationship = new RelationshipService('localhost:50051', grpc.credentials.createInsecure());
+const client_chat = new ChatService('localhost:50051', grpc.credentials.createInsecure());
 
 // Клиентские методы
 const authClient = {
@@ -238,5 +241,21 @@ const searchClient = {
         });
     }
 };
+
+const chatClient = {
+    createChatDM: async ({ current_user, target_user }) => {
+        const request = {
+            current_user,
+            target_user
+        }
+
+        return new Promise((resolve, reject) => {
+            client_chat.createChatDM(request, (err, response) => {
+                if (err) return reject(err);
+                resolve(response);
+            })
+        })
+    }
+}
 
 module.exports = { authClient, relationshipClient, searchClient, statusClient };
